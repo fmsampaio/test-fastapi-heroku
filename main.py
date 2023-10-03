20229b4e9dd2a64f96d808d902ea8f1cd26560e9
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2023-10-03 10:33:12 -0300

    Added endpoint for randomly getting student name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+import random
+from datetime import datetime
+
 app = FastAPI()
 
 @app.get("/")
@@ -7,3 +10,45 @@ def index():
     return {
         "data" : "Simple FastAPI app for Heroku deployment (updated)!"
     }
+
+students = [
+    'ANA CAROLINA DA SILVA',
+    'ANTÔNIO BERTHES PASTORI',
+    'ARTHUR COLOGNESE BUSETTI',
+    'DAVI VALÉRIO MACEDO',
+    'DIEGO NATAN DA SILVA',
+    'DIOGO CERENTINI',
+    'EDUARDO ERNESTO PICCOLI',
+    'EDUARDO STEIN GUIZZO',
+    'ENZO DANIEL ZARDO',
+    'FELIPE MÁRCIO FURLAN MAINARDO CARDOSO',
+    'GABRIEL LOVATO VIANNA',
+    'GRAZIELE LOPES DE SOUZA',
+    'GUILHERME PASQUAL DE TONI',
+    'GUSTAVO DE CESERO VIEIRA',
+    'IURY RIBEIRO IANOSKI',
+    'JOSÉ HENRIQUE NOAL',
+    'KARINA MARQUES BIANCHI',
+    'LÚCIO MARIO SILVESTRIN',
+    'MATEUS MATANA',
+    'MATEUS ROCHA BATTÚ',
+    'MIKAEL FERNANDES MOREIRA',
+    'MURILO BOEIRA DUTRA',
+    'NICOLAS SARAIVA',
+    'PIETRO DE OLIVEIRA BORTOLINI',
+    'SAMIRA MARQUES',
+    'SANDERSON GUGGIANA PINARELO',
+    'VICTOR AUGUSTO MESNEROVICZ',
+    'VITHÓRIA SOSNOSKI MATOS',
+]
+
+@app.get("/student/")
+def getStudentName():
+    studentName = students[random.randint(0, len(students))]
+    dt = datetime.now()
+
+    return {
+        'timestamp' : dt.strftime("%d-%m,%H:%M:%S"),
+        'student_name' : studentName
+    }
+
